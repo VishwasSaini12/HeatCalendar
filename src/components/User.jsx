14fc198c11112158d212547ac508b6f75172dcee
Washim-Bari <washim.bari@oaksol.in>
2024-05-09 12:28:52 +0530

    lead-board chart

---
+++ src/components/User.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function User({ name, percentage , position , height, imgSrc}) {
+	return (
+		<div className="user-bar">
+			<img src={imgSrc} alt={name} className="user-image" />
+			<div className="user-percentage">{percentage}</div>
+			<div className="user-name">{name}</div>
+			<div className="user" style={{height: height}}>
+				<div className="user-profile">
+          <div className="user-details">#{ position}</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default User;
